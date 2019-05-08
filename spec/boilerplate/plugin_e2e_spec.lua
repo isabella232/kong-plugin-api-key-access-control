7039f6de1c8084274fc397eb7e88c0339e41a4d5
bencso <gabor.bencsik13@gmail.com>
2019-05-08 13:40:20 +0200

    CDPIP-OPS: Refact tests to use kong_client test helper

--- spec/boilerplate/plugin_e2e_spec.lua
+++ spec/boilerplate/plugin_e2e_spec.lua
@@ -1,9 +1,15 @@
 local helpers = require "spec.helpers"
-local cjson = require "cjson"
+local kong_client = require "kong_client.spec.test_helpers"
 
 describe("Boilerplate", function()
+  local kong_sdk, send_request, send_admin_request
+
   setup(function()
-    helpers.start_kong({ custom_plugins = 'boilerplate' })
+    helpers.start_kong({ custom_plugins = "boilerplate" })
+
+    kong_sdk = kong_client.create_kong_client()
+    send_request = kong_client.create_request_sender(helpers.proxy_client())
+    send_admin_request = kong_client.create_request_sender(helpers.admin_client())
   end)
 
   teardown(function()
@@ -15,134 +21,68 @@ describe("Boilerplate", function()
   end)
 
   context('when the "say_hello" flag is true', function()
+
+    local service
+
+    before_each(function()
+      service = kong_sdk.services:create({
+        name = "test-service",
+        url = "http://mockbin:8080/request"
+      })
+
+      kong_sdk.routes:create_for_service(service.id, "/test")
+    end)
+
     it('should add headers to the proxied request', function()
-      local service_creation_call = assert(helpers.admin_client():send({
-        method = 'POST',
-        path = '/services',
-        body = {
-          name = 'MockBin',
-          url = 'http://mockbin:8080/request'
-        },
-        headers = {
-          ['Content-Type'] = 'application/json'
+      kong_sdk.plugins:create({
+        service_id = service.id,
+        name = "boilerplate",
+        config = {
+          say_hello = true
         }
-      }))
+      })
 
-      local service_creation_data = cjson.decode(
-        assert.res_status(201, service_creation_call)
-      )
+      local response = send_request({
+        method = "GET",
+        path = "/test"
+      })
 
-      local service_id = service_creation_data.id
-
-      local route_creation_call = assert(helpers.admin_client():send({
-        method = 'POST',
-        path = '/services/' .. service_id .. '/routes',
-        body = {
-          paths = {
-            '/test'
-          }
-        },
-        headers = {
-          ['Content-Type'] = 'application/json'
-        }
-      }))
-
-      assert.res_status(201, route_creation_call)
-
-      local plugin_creation_call = assert(helpers.admin_client():send({
-        method = 'POST',
-        path = '/services/' .. service_id .. '/plugins',
-        body = {
-          name = 'boilerplate',
-          config = {
-            say_hello = true
-          }
-        },
-        headers = {
-          ['Content-Type'] = 'application/json'
-        }
-      }))
-
-      assert.res_status(201, plugin_creation_call)
-
-      local client_call = assert(helpers.proxy_client():send({
-        method = 'GET',
-        path = '/test'
-      }))
-
-      local client_response_data = cjson.decode(
-        assert.res_status(200, client_call)
-      )
-
-      assert.is_equal('Hey Upstream!', client_response_data.headers['x-upstream-header'])
-      assert.response(client_call).has.header('X-Downstream-Header')
-      assert.is_equal('Hey Downstream!', client_call.headers['X-Downstream-Header'])
+      assert.are.equal(200, response.status)
+      assert.is_equal('Hey Upstream!', response.body.headers['x-upstream-header'])
+      assert.is_equal('Hey Downstream!', response.headers['X-Downstream-Header'])
     end)
   end)
 
   context('when the "say_hello" flag is false', function()
+
+    local service
+
+    before_each(function()
+      service = kong_sdk.services:create({
+        name = "test-service",
+        url = "http://mockbin:8080/request"
+      })
+
+      kong_sdk.routes:create_for_service(service.id, "/test")
+    end)
+
     it('should add headers to the proxied request', function()
-      local service_creation_call = assert(helpers.admin_client():send({
-        method = 'POST',
-        path = '/services',
-        body = {
-          name = 'MockBin',
-          url = 'http://mockbin:8080/request'
-        },
-        headers = {
-          ['Content-Type'] = 'application/json'
+      kong_sdk.plugins:create({
+        service_id = service.id,
+        name = "boilerplate",
+        config = {
+          say_hello = false
         }
-      }))
+      })
 
-      local service_creation_data = cjson.decode(
-        assert.res_status(201, service_creation_call)
-      )
+      local response = send_request({
+        method = "GET",
+        path = "/test"
+      })
 
-      local service_id = service_creation_data.id
-
-      local route_creation_call = assert(helpers.admin_client():send({
-        method = 'POST',
-        path = '/services/' .. service_id .. '/routes',
-        body = {
-          paths = {
-            '/test'
-          }
-        },
-        headers = {
-          ['Content-Type'] = 'application/json'
-        }
-      }))
-
-      assert.res_status(201, route_creation_call)
-
-      local plugin_creation_call = assert(helpers.admin_client():send({
-        method = 'POST',
-        path = '/services/' .. service_id .. '/plugins',
-        body = {
-          name = 'boilerplate',
-          config = {
-            say_hello = false
-          }
-        },
-        headers = {
-          ['Content-Type'] = 'application/json'
-        }
-      }))
-
-      assert.res_status(201, plugin_creation_call)
-
-      local client_call = assert(helpers.proxy_client():send({
-        method = 'GET',
-        path = '/test'
-      }))
-
-      local client_response_data = cjson.decode(
-        assert.res_status(200, client_call)
-      )
-
-      assert.is_equal('Bye Upstream!', client_response_data.headers['x-upstream-header'])
-      assert.response(client_call).has.header('X-Downstream-Header')
-      assert.is_equal('Bye Downstream!', client_call.headers['X-Downstream-Header'])
+      assert.are.equal(200, response.status)
+      assert.is_equal('Bye Upstream!', response.body.headers['x-upstream-header'])
+      assert.is_equal('Bye Downstream!', response.headers['X-Downstream-Header'])
     end)
   end)
 end)
