d6c924a560c0880392e2246fb49f5aaa9fb65182
Gabor Barnak <gabor.barnak@gmail.com>
2019-11-25 16:22:31 +0100

    CDPINT-1162: Refactor step

--- kong/plugins/api-key-access-control/handler.lua
+++ kong/plugins/api-key-access-control/handler.lua
@@ -4,25 +4,45 @@ local ApiKeyAccessControlHandler = BasePlugin:extend()
 
 ApiKeyAccessControlHandler.PRIORITY = 950
 
+local function compose_rule_from_context(api_key)
+  local method = kong.request.get_method()
+  local path = kong.request.get_path()
+  return table.concat({api_key, method, path}, " ")
+end
+
+local function is_key_in_list_of_keys(key, list_of_keys)
+  for i = 1, #list_of_keys do
+    if list_of_keys[i] == key then
+      return true
+    end
+  end
+  return false
+end
+
+local function is_rule_in_whitelist(rule, whitelist)
+  for i = 1, #whitelist do
+    if whitelist[i] == rule then
+      return true
+    end
+  end
+  return false
+end
+
 function ApiKeyAccessControlHandler:new()
   ApiKeyAccessControlHandler.super.new(self, "api-key-access-control")
 end
 
-function ApiKeyAccessControlHandler:access(conf)
+function ApiKeyAccessControlHandler:access(config)
   ApiKeyAccessControlHandler.super.access(self)
 
   local api_key = kong.request.get_header("x_credential_username")
-  local whitelist_rule = table.concat({api_key, kong.request.get_method(), kong.request.get_path()}, " ")
+  local rule = compose_rule_from_context(api_key)
 
-  for i = 1, #conf.api_keys do
-    if conf.api_keys[i] == api_key then
-      for j = 1, #conf.whitelist do
-        if whitelist_rule == conf.whitelist[j] then
-          return
-        end
-      end
-      return kong.response.exit(403)
+  if is_key_in_list_of_keys(api_key, config.api_keys) then
+    if is_rule_in_whitelist(rule, config.whitelist) then
+      return
     end
+    return kong.response.exit(403)
   end
 
   return
